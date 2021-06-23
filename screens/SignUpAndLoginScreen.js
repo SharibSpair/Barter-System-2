51889a18354f258b5f50d36cbb236fec835b6dca
SharibShaikhClass <shaikhsharib463@gmail.com>
2021-06-23 11:56:21 +0530

    first ommit

--- screens/SignUpAndLoginScreen.js
+++ screens/SignUpAndLoginScreen.js
@@ -5,10 +5,13 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Image
 } from "react-native";
 import firebase from "firebase";
 import db from "../config";
 import { Alert } from "react-native";
+import { ScrollView } from "react-native";
+import { KeyboardAvoidingView,Modal } from "react-native";
 
 export default class SignUpAndLoginScreen extends React.Component {
   constructor() {
@@ -16,6 +19,12 @@ export default class SignUpAndLoginScreen extends React.Component {
     this.state = {
       emailId: "",
       password: "",
+      firstName: "",
+      lastName: "",
+      mobileNo: "",
+      address: "",
+      confirmpassword: "",
+      isModalVisible: true
     };
   }
 
@@ -33,7 +42,11 @@ export default class SignUpAndLoginScreen extends React.Component {
       });
   };
 
-  userSignUp = (emailId, password) => {
+  userSignUp = (emailId, password, confirmpassword) => {
+    if(password !== confirmpassword){
+      return Alert.alert("Passwords Do Not Match")
+    }
+    else{
     firebase
       .auth()
       .createUserWithEmailAndPassword(emailId, password)
@@ -45,11 +58,136 @@ export default class SignUpAndLoginScreen extends React.Component {
         var errormessage = error.message;
         return Alert.alert(errormessage);
       });
-  };
+  }
+};
+
+showmodal = () => {
+  return <Modal animationType = {'fade'} 
+  transparent = {true}
+  visible = {this.state.isModalVisible}
+  >
+  <View style = {styles.modalContainer}>
+    
+    <ScrollView style = {{width:'100%'}}>
+<KeyboardAvoidingView style={styles.keyboardview}>
+
+  <Text style = {styles.modalTitle}>Registration</Text>
+<TextInput 
+  style={styles.modaltextinput}
+  placeholder="First Name"
+  maxLength = {10}
+onChangeText = {(text)=>{
+  this.setState({
+    firstName:text
+  })
+}}
+/>
+<TextInput 
+  style={styles.modaltextinput}
+  placeholder="Last Name"
+  maxLength = {10}
+onChangeText = {(text)=>{
+  this.setState({
+    lastName:text
+  })
+}}
+/>
+
+<TextInput 
+  style={styles.modaltextinput}
+  placeholder="Contact No."
+  maxLength = {10}
+  keyboardType='numeric'
+onChangeText = {(text)=>{
+  this.setState({
+    mobileNo:text
+  })
+}}
+/>
+
+<TextInput 
+  style={styles.modaltextinput}
+  placeholder="Address"
+  multiline={true}
+ onChangeText = {(text)=>{
+  this.setState({
+    address:text
+  })
+}}
+/>
+
+<TextInput 
+  style={styles.modaltextinput}
+  placeholder="Email ID"
+  keyboardType="email-address"
+  onChangeText = {(text)=>{
+  this.setState({
+    emailId:text
+  })
+}}
+/>
+<TextInput 
+  style={styles.modaltextinput}
+  placeholder="Password"
+  secureTextEntry= {true}
+ onChangeText = {(text)=>{
+  this.setState({
+    password:text
+  })
+}}
+/>
+
+<TextInput 
+  style={styles.modaltextinput}
+  placeholder="Confirm Password"
+  secureTextEntry= {true}
+ onChangeText = {(text)=>{
+  this.setState({
+    confirmPassword:text
+  })
+}}
+/>
+
+</KeyboardAvoidingView>
+
+<TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          this.userSignUp(this.state.emailId, this.state.password,this.state.confirmPassword);
+        }}
+      >
+        <Text style={styles.buttontext}>Register</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+         this.setState({isModalVisible:false})
+        }}
+      >
+        <Text style={styles.buttontext}>Cancel</Text>
+      </TouchableOpacity>
+    </ScrollView>
+
+  </View>
+    
+  </Modal>;
+};
+
 
   render() {
     return (
       <View style={styles.container}>
+         <View style = {{justifyContent:'center',alignItems:'center'}}> 
+    {
+      this.showmodal()
+    }
+
+   </View>
+   <Image
+          source={require("../assets/barter.png")}
+          style={{ width: 200, height: 200, marginBottom: 40 }}
+        />
         <View>
           <Text style={styles.title}> Barter </Text>
         </View>
@@ -103,10 +241,13 @@ export default class SignUpAndLoginScreen extends React.Component {
   }
 }
 
+
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgb(251,229,183)",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -116,12 +257,14 @@ const styles = StyleSheet.create({
     fontWeight: "100",
     //paddingBottom: 30,
     color: "orange",
+    fontWeight:'bold'
   },
   belowtitle: {
     fontSize: 10,
 
     // marginBottom:200,
     color: "orange",
+    fontWeight:'bold'
   },
   loginBox: {
     width: 200,
@@ -167,9 +310,110 @@ const styles = StyleSheet.create({
     shadowRadius: 10.32,
     elevation: 16,
     marginTop: 30,
+    fontWeight:'bold'
   },
   submittext: {
     fontSize: 16,
     color: "orange",
+    fontWeight:'bold'
   },
+  modalTitle:{
+    justifyContent:'center',
+    alignSelf:'center',
+    fontSize:30,
+    color:'orange',
+    margin:50
+  },
+  
+  modalContainer:{
+  flex:1,
+  justifyContent:'center',
+  alignItems:'center',
+  backgroundColor:'white',
+  marginRight:30,
+  marginLeft:30,
+  marginTop:30,
+  marginBottom:60,
+  borderWidth:1,
+  borderRadius:20,
+
+  },
+
+  keyboardview:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  },
+
+  modaltextinput:{
+    width:"75%",
+    height:35,
+    alignSelf:'center',
+    borderColor:'orange',
+    borderRadius:10,
+    borderWidth:1,
+    marginTop:20,
+    padding:10
+  
+
+},
+  registerbutton:{
+    width:200,
+   height:40,
+   alignItems:'center',
+   justifyContent:'center',
+   borderWidth:1,
+   borderRadius:10,
+   marginTop:30
+  },
+
+  registerbuttontext:{
+    color:'#ff5722',
+    fontSize:15,
+    fontWeight:'bold'
+  },
+
+  cancelButton:{
+    width:200,
+   height:30,
+   justifyContent:'center',
+   alignItems:'center',
+   marginTop:5,
+  },
+  
+  cancelbuttontext:{
+    color:'#ff5722',
+    fontSize:15,
+
+  },
+ 
+    button:{
+      width:200,
+      height:30,
+      justifyContent:'center',
+      alignSelf:'center',
+      alignItems:'center',
+      borderRadius:25,
+      backgroundColor:"orange",
+      shadowColor: "#000",
+      marginTop:20,
+    
+      shadowOffset: {
+         width: 0,
+         height: 8,
+      },
+      shadowOpacity: 0.30,
+      shadowRadius: 10.32,
+      elevation: 16,
+      padding: 10
+    },
+
+    buttonText:{
+      color:'#ffff',
+      fontWeight:'200',
+      fontSize:20,
+      fontWeight:'bold'
+    }
+  
+
 });
